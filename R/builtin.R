07ca4eb9fab1a2f2636b8725d2766231d6ab39f0
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2026-02-14 16:14:37 -0800

    Cleanups

--- R/builtin.R
+++ R/builtin.R
@@ -7,55 +7,62 @@
 #   }, .progressr_progressor = progressr::progressor(along = .progressr_along))
 # )
 #
-progressify_base <- function(expr, fcn_name, fcn, ..., envir = parent.frame()) {
-  names <- names(expr)
-  if (is.null(names)) names <- rep("", length.out = length(expr))
-  names <- names[-1]
-  target_names <- names(formals(fcn))[seq_along(names)]
-  unnamed <- setdiff(target_names, names)
-  names[names == ""] <- unnamed
-  names <- c("", names)
+progressify_base <- local({
+  function(expr, fcn_name, fcn, ..., envir = parent.frame()) {
+    names <- names(expr)
+    if (is.null(names)) names <- rep("", length.out = length(expr))
+    names <- names[-1]
+    target_names <- names(formals(fcn))[seq_along(names)]
+    unnamed <- setdiff(target_names, names)
+    names[names == ""] <- unnamed
+    names <- c("", names)
 
-  if (fcn_name %in% c("eapply")) {
-    idx_X <- which(names == "env")
-    stopifnot(length(idx_X) == 1L)
-    X <- expr[[idx_X]]
-    idx_FUN <- which(names == "FUN")
-    stopifnot(length(idx_FUN) == 1L)
-    FUN <- expr[[idx_FUN]]
-  } else if (fcn_name %in% c("lapply", "sapply", "vapply")) {
-    idx_X <- which(names == "X")
-    stopifnot(length(idx_X) == 1L)
-    X <- expr[[idx_X]]
-    idx_FUN <- which(names == "FUN")
-    stopifnot(length(idx_FUN) == 1L)
-    FUN <- expr[[idx_FUN]]
-  } else if (fcn_name %in% c("replicate")) {
-    stop("Not implemented")
-  } else {
-    idx_FUN <- which(names == "FUN")
-    stopifnot(length(idx_FUN) == 1L)
-    FUN <- expr[[idx_FUN]]
-  }
+    if (fcn_name %in% c("eapply")) {
+      idx_X <- which(names == "env")
+      idx_FUN <- which(names == "FUN")
+    } else if (fcn_name %in% c("lapply", "sapply", "vapply")) {
+      idx_X <- which(names == "X")
+      idx_FUN <- which(names == "FUN")
+    } else if (fcn_name %in% c("replicate")) {
+      stop("Not implemented")
+    } else {
+      idx_X <- NULL
+      idx_FUN <- which(names == "FUN")
+    }
 
-  t_FUN <- bquote(function(..., .progressr_progressor) {
-    on.exit(.progressr_progressor())
-    .(FUN)(...)
-  })
-  parts <- as.list(expr)
-  parts[[idx_X]] <- bquote({ .progressr_along <- .(parts[[idx_X]]) })
-  parts[[idx_FUN]] <- t_FUN
-  progressr_args <- list(
-    .progressr_progressor = quote(progressr::progressor(along = .progressr_along))
-  )
-  parts <- c(parts, progressr_args)
-  t_expr <- bquote(local(.(as.call(parts))))
+    parts <- as.list(expr)
 
-  t_expr
-} ## progressify_base()
+    if (!is.null(idx_X)) {
+      stopifnot(length(idx_X) == 1L)
+      parts[[idx_X]] <- bquote({ .progressr_along <- .(parts[[idx_X]]) })
+    }
+
+    if (!is.null(idx_FUN)) {
+      stopifnot(length(idx_FUN) == 1L)
+      FUN <- expr[[idx_FUN]]
+      t_FUN <- bquote(function(..., .progressr_progressor) {
+        on.exit(.progressr_progressor())
+        .(FUN)(...)
+      })
+      parts[[idx_FUN]] <- t_FUN
+    }
+
+    progressr_args <- list(
+      .progressr_progressor = quote(progressr::progressor(along = .progressr_along))
+    )
+    parts <- c(parts, progressr_args)
+    t_expr <- bquote(local(.(as.call(parts))))
+  
+    t_expr
+  } ## progressify_base()
+})
 
 
 append_builtin_transpilers_for_base <- local({
+  append_transpilers <- import_futurize("append_transpilers")
+  bquote_compile <- import_futurize("bquote_compile")
+  bquote_apply <- import_futurize("bquote_apply")
+  
   known_fcns <- list(
     apply = c,
     by = c,
@@ -70,30 +77,31 @@ append_builtin_transpilers_for_base <- local({
     vapply = c              ## done
   )
 
-  append_transpilers <- import_from("append_transpilers", package = "futurize")
+  template <- bquote_compile(function(expr, options) {
+    fcn <- get(.(fcn_name), mode = "function", envir = baseenv())
+    progressify_base(expr, fcn_name = .(fcn_name), fcn = fcn, envir = parent.frame())
+  })
+
+  make_transpiler <- function(fcn_name) {
+    transpiler <- eval(bquote_apply(template))
+    eval(transpiler)
+  }
+
   function() {
     ## base::apply(), ...
     transpilers <- list()
-  
-    ## Create all transpilers
     for (fcn_name in names(known_fcns)) {
-      label <- sprintf("base::%s() transpiler", fcn_name)
-      make_transpiler_expr <- bquote(function(expr, options) {
-        fcn_name <- .(fcn_name)
-        fcn <- get(fcn_name, mode = "function", envir = baseenv())
-        progressify_base(expr, fcn_name = fcn_name, fcn = fcn, envir = parent.frame())
-      })
-      transpiler <- eval(make_transpiler_expr)
       transpilers[[fcn_name]] <- list(
-        label = label,
-        transpiler = transpiler
+        label = sprintf("base::%s() transpiler", fcn_name),
+        transpiler = make_transpiler(fcn_name)
       )
     } ## for (fcn_name ...)
+    transpilers <- list(base = transpilers)
     
-    append_transpilers("progressify::built-in", list(base = transpilers))
+    append_transpilers("progressify::built-in", transpilers)
     
     ## Return required packages
-    character(0L)
+    c("base", "progressr")
   }
 })
 
